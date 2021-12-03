c1128e6669fd59d3e0ede17df94957160ff66de2
jdvr <juandavidvegarguez@gmail.com>
2021-12-03 02:03:27 +0100

    Day 2 - Part 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -3,7 +3,9 @@ package day02
 import readLinesAs
 
 data class Command(val direction: String, val amount: Int)
-
+data class Position(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0) {
+    fun horizontalByDepth(): Int = horizontal * depth
+}
 
 // What do you get if you multiply your final horizontal position by your final depth?
 fun main() {
@@ -13,10 +15,12 @@ fun main() {
     }.toTypedArray()
     val (horizontal, depth) = calculatePosition(commands)
     println("$horizontal * $depth = ${horizontal * depth}")
+    val positionWithAim = calculatePositionWithAim(commands)
+    println("${positionWithAim.horizontal} * ${positionWithAim.depth} = ${positionWithAim.horizontalByDepth()}")
 }
 
 fun calculatePosition(commands: Array<Command>): Pair<Int, Int> {
-    return commands.fold(Pair(0,0)) { acc, command ->
+    return commands.fold(Pair(0, 0)) { acc, command ->
         when (command.direction) {
             "forward" -> acc.copy(first = acc.first + command.amount)
             "down" -> acc.copy(second = acc.second + command.amount)
@@ -26,11 +30,18 @@ fun calculatePosition(commands: Array<Command>): Pair<Int, Int> {
     }
 }
 
-
-
-class Day02Test {
-
-    fun `for test input returns 150`() {
-
+fun calculatePositionWithAim(commands: Array<Command>): Position {
+    return commands.fold(Position()) { acc, command ->
+        when (command.direction) {
+            "forward" ->
+                if (acc.aim == 0) {
+                    acc.copy(horizontal = acc.horizontal + command.amount)
+                } else {
+                    acc.copy(horizontal = acc.horizontal + command.amount, depth = acc.aim * command.amount + acc.depth)
+                }
+            "down" -> acc.copy(aim = acc.aim + command.amount)
+            "up" -> acc.copy(aim = acc.aim - command.amount)
+            else -> acc
+        }
     }
 }
--- src/day02/Day02Tests.kt
+++ src/day02/Day02Tests.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class Day02Tests {
     @Test
-    fun `for test input returns 150`() {
+    fun `part 1 for test input returns 150`() {
         val (horizontal, depth) = calculatePosition(arrayOf(
             Command("forward", 5),
             Command("down", 5),
@@ -17,5 +17,19 @@ class Day02Tests {
         ))
 
         assertThat(horizontal * depth).isEqualTo(150)
+    }
+
+    @Test
+    fun `part 2 for test input returns 150`() {
+        val (horizontal, depth, aim) = calculatePositionWithAim(arrayOf(
+            Command("forward", 5),
+            Command("down", 5),
+            Command("forward", 8),
+            Command("up", 3),
+            Command("down", 8),
+            Command("forward", 2),
+        ))
+
+        assertThat(horizontal * depth).isEqualTo(900)
     }
 }
