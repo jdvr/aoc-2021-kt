024af5730c035a2eb6d6d37289e8ebada05f580b
jdvr <juandavidvegarguez@gmail.com>
2021-12-03 02:49:54 +0100

    Day 2 - Part 2 - without explicit conditional

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -2,46 +2,97 @@ package day02
 
 import readLinesAs
 
-data class Command(val direction: String, val amount: Int)
-data class Position(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0) {
+
+data class ThreeDimensionPosition(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0) {
+
+    fun forward(amount: Int): ThreeDimensionPosition = if (aim == 0) {
+        copy(horizontal = horizontal + amount)
+    } else {
+        copy(horizontal = horizontal + amount, depth = aim * amount + depth)
+    }
+
+    fun down(amount: Int): ThreeDimensionPosition = copy(aim = aim + amount)
+
+    fun up(amount: Int): ThreeDimensionPosition = copy(aim = aim - amount)
+
+    fun then(c: Command): ThreeDimensionPosition = c.on(this)
+
     fun horizontalByDepth(): Int = horizontal * depth
+
+}
+
+data class TwoDimensionPosition(val horizontal: Int = 0, val depth: Int = 0) {
+
+    fun forward(amount: Int): TwoDimensionPosition = copy(horizontal = horizontal + amount)
+
+    fun down(amount: Int): TwoDimensionPosition = copy(depth = depth + amount)
+
+    fun up(amount: Int): TwoDimensionPosition = copy(depth = depth - amount)
+
+    fun then(c: Command): TwoDimensionPosition = c.on(this)
+
+    fun horizontalByDepth(): Int = horizontal * depth
+
+}
+
+typealias CFactory = (a: Int) -> Command
+
+interface Command {
+    fun amount(): Int
+    fun on(p: ThreeDimensionPosition): ThreeDimensionPosition
+    fun on(p: TwoDimensionPosition): TwoDimensionPosition
+}
+
+data class Forward(private val amount: Int) : Command {
+    override fun amount(): Int = amount
+    override fun on(p: ThreeDimensionPosition): ThreeDimensionPosition = p.forward(amount)
+    override fun on(p: TwoDimensionPosition): TwoDimensionPosition = p.forward(amount)
+}
+
+data class Down(private val amount: Int) : Command {
+    override fun amount(): Int = amount
+    override fun on(p: ThreeDimensionPosition): ThreeDimensionPosition = p.down(amount)
+    override fun on(p: TwoDimensionPosition): TwoDimensionPosition = p.down(amount)
+}
+
+data class Up(private val amount: Int) : Command {
+    override fun amount(): Int = amount
+    override fun on(p: ThreeDimensionPosition): ThreeDimensionPosition = p.up(amount)
+    override fun on(p: TwoDimensionPosition): TwoDimensionPosition = p.up(amount)
+}
+
+val FactoryByCommandName = mapOf<String, CFactory>(
+    "forward" to { Forward(it) },
+    "down" to { Down(it) },
+    "up" to { Up(it) }
+)
+
+fun calculatePosition(commands: Array<Command>): TwoDimensionPosition {
+    return commands.fold(TwoDimensionPosition()) { acc, c ->
+        acc.then(c)
+    }
+}
+
+fun calculatePositionWithAim(commands: Array<Command>): ThreeDimensionPosition {
+    return commands.fold(ThreeDimensionPosition()) { acc, c ->
+        acc.then(c)
+    }
+}
+
+fun mapLine(it: String): Command {
+    val (command, amount) = it.trim().split(" ")
+    return FactoryByCommandName[command]!!.invoke(amount.toInt())
 }
 
 // What do you get if you multiply your final horizontal position by your final depth?
 fun main() {
     val commands = readLinesAs("day02/Day02") {
-        val (command, amount) = it.trim().split(" ")
-        Command(command, amount.toInt())
+        mapLine(it)
     }.toTypedArray()
-    val (horizontal, depth) = calculatePosition(commands)
-    println("$horizontal * $depth = ${horizontal * depth}")
+
+    val position = calculatePosition(commands)
+    println("${position.horizontal} * ${position.depth} = ${position.horizontalByDepth()}")
     val positionWithAim = calculatePositionWithAim(commands)
     println("${positionWithAim.horizontal} * ${positionWithAim.depth} = ${positionWithAim.horizontalByDepth()}")
 }
 
-fun calculatePosition(commands: Array<Command>): Pair<Int, Int> {
-    return commands.fold(Pair(0, 0)) { acc, command ->
-        when (command.direction) {
-            "forward" -> acc.copy(first = acc.first + command.amount)
-            "down" -> acc.copy(second = acc.second + command.amount)
-            "up" -> acc.copy(second = acc.second - command.amount)
-            else -> acc
-        }
-    }
-}
-
-fun calculatePositionWithAim(commands: Array<Command>): Position {
-    return commands.fold(Position()) { acc, command ->
-        when (command.direction) {
-            "forward" ->
-                if (acc.aim == 0) {
-                    acc.copy(horizontal = acc.horizontal + command.amount)
-                } else {
-                    acc.copy(horizontal = acc.horizontal + command.amount, depth = acc.aim * command.amount + acc.depth)
-                }
-            "down" -> acc.copy(aim = acc.aim + command.amount)
-            "up" -> acc.copy(aim = acc.aim - command.amount)
-            else -> acc
-        }
-    }
-}
--- src/day02/Day02Tests.kt
+++ src/day02/Day02Tests.kt
@@ -5,30 +5,36 @@ import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test;
 
 class Day02Tests {
+    private val testInput = arrayOf(
+        Forward(5),
+        Down(5),
+        Forward(8),
+        Up(3),
+        Down(8),
+        Forward(2),
+    )
+
+    @Test
+    fun `map a line`() {
+        assertThat(mapLine("forward 4")).isEqualTo(Forward(4))
+        assertThat(mapLine("down 6")).isEqualTo(Down(6))
+        assertThat(mapLine("up 9")).isEqualTo(Up(9))
+    }
+
     @Test
     fun `part 1 for test input returns 150`() {
-        val (horizontal, depth) = calculatePosition(arrayOf(
-            Command("forward", 5),
-            Command("down", 5),
-            Command("forward", 8),
-            Command("up", 3),
-            Command("down", 8),
-            Command("forward", 2),
-        ))
+        val (horizontal, depth) = calculatePosition(
+            testInput
+        )
 
         assertThat(horizontal * depth).isEqualTo(150)
     }
 
     @Test
     fun `part 2 for test input returns 150`() {
-        val (horizontal, depth, aim) = calculatePositionWithAim(arrayOf(
-            Command("forward", 5),
-            Command("down", 5),
-            Command("forward", 8),
-            Command("up", 3),
-            Command("down", 8),
-            Command("forward", 2),
-        ))
+        val (horizontal, depth, aim) = calculatePositionWithAim(
+            testInput
+        )
 
         assertThat(horizontal * depth).isEqualTo(900)
     }
